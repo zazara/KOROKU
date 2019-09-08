b78a8906f7980c73cae5ce09d02acd3be9eb2399
zazara <porinum1@gmail.com>
2019-09-08 21:19:39 +0900

    Add resource files

--- src/main.cpp
+++ src/main.cpp
@@ -27,7 +27,8 @@ int main(int argc, char **argv) {
   auto app = Gtk::Application::create(argc, argv);
 
   try {
-    refBuilder = Gtk::Builder::create_from_file("src/glade_project.glade");
+    // refBuilder = Gtk::Builder::create_from_file("src/glade_project.glade");
+    refBuilder = Gtk::Builder::create_from_resource("/ui/glade_project.glade");
   } catch (const Glib::FileError &ex) {
     std::cerr << "FileError: " << ex.what() << std::endl;
     return 1;
@@ -39,7 +40,7 @@ int main(int argc, char **argv) {
   Gtk::Box *side_menu = nullptr;
   refBuilder->get_widget("side_menu", side_menu);
   Glib::RefPtr<Gtk::CssProvider> css_provider = Gtk::CssProvider::create();
-  css_provider->load_from_path("src/style.css");
+  css_provider->load_from_resource("/ui/style.css");
   side_menu->get_style_context()->add_provider(
       css_provider, GTK_STYLE_PROVIDER_PRIORITY_USER);
 
