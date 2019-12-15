05d05b80cd1bcf11cf8c8467e7e5e45c70ca1bc9
zazara <porinum1@gmail.com>
2019-12-15 21:52:45 +0900

    Fix: layer clear bug

--- src/Canvas.h
+++ src/Canvas.h
@@ -410,11 +410,15 @@ bool Canvas::set_background_opacity(Gtk::ScrollType scroll, double value) {
 }
 
 void Canvas::clear_layer() {
-  for (long unsigned brush_idx = 0; brush_idx < this->brushes.size();
-       brush_idx++) {
+  long unsigned brush_idx = this->brushes.size() - 1;
+  while (true) {
     if (this->brushes[brush_idx].brush_layer == this->layer_select) {
       this->brushes.erase(this->brushes.begin() + brush_idx);
     }
+    if (brush_idx == 0) {
+      break;
+    }
+    brush_idx--;
   }
   this->queue_draw();
 }
